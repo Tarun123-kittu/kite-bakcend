4b0474883cbfab5a7a914b37629db22f3176d642
tarun.gautam@ultivic.com <tarun.gautam@ultivic.com>
2022-08-22 11:05:13 +0530

    abstract added

--- src/app/features/Abstract/AbstractSlice.js
+++ src/app/features/Abstract/AbstractSlice.js
@@ -1,2 +1,77 @@
-import {createSlice , createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from "axios";
+
+export const fetchabstract = createAsyncThunk(
+    'abstract/fetchabstract',
+    async ({ gender, age, country, token }, thunkAPI) => {
+        try {
+            return await axios.post(`${process.env.REACT_APP_BASE_URL}v1/abstract`, { gender, age, country }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "Bearer " + token,
+                }
+            }).then((data) => {
+                return { ...data.data }
+            }).catch((e) => {
+                return thunkAPI.rejectWithValue(e.response.data);
+            })
+
+        } catch (e) {
+            console.log(e)
+            let error = "";
+            if (e.response) {
+                error = e.response.data;
+            } else if (e.request) {
+                error = e.request;
+            } else {
+                error = e.message;
+            }
+            return thunkAPI.rejectWithValue(error)
+        }
+    }
+)
+
+export const AbstractSlice = createSlice({
+    name: "abstract",
+    initialState: {
+        incidencia: {},
+        poblacion_proyectada: {},
+        population_projection_by_age: {},
+        use_as_per_age: {},
+        status: '',
+        isFetching: false,
+        isSuccess: false,
+        isError: false,
+        errorMessage: ''
+    },
+    reducers: {
+        clearState: (state) => {
+            state.isError = false;
+            state.isSuccess = false;
+            state.isFetching = false;
+            return state;
+        },
+    },
+    extraReducers: {
+        [fetchabstract.fulfilled]: (state, { payload }) => {
+            state.isFetching = false;
+            state.isSuccess = true;
+            state.status = payload.message;
+            state.incidencia = payload.data.incidencia;
+            state.poblacion_proyectada = payload.data.poblacion_proyectada;
+            state.population_projection_by_age = payload.data.population_projection_by_age;
+            state.use_as_per_age = payload.data.use_as_per_age;
+        },
+        [fetchabstract.pending]: (state) => {
+            state.isFetching = true;
+        },
+        [fetchabstract.rejected]: (state, { payload }) => {
+            state.isFetching = false;
+            state.isError = true;
+            state.errorMessage = payload.message;
+        },
+    }
+})
+export const { clearState } = AbstractSlice.actions;
+
+export const abstractSelector = (state) => state.abstract;  
